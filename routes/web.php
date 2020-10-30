fef19f1c0473c59c0b5dd1679edd26bf6ba12d37
wangshengsheng0215 <1422206528@qq.com>
2020-10-30 19:44:51 +0800

    ss

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@
 |
 */
 Route::get('slice/test','Web\TestController@slicetest');
-//Route::get('/', function () {return view('welcome');}); //欢迎页面
+Route::get('/', function () {return view('welcome');}); //欢迎页面
 Route::post('web/vranchor/register','Web\LoginController@register');
 //Route::get('/', function () {return view('homepage');}); //首页
 Route::get('test','Web\TestController@test');//测试
@@ -19,17 +19,20 @@ Route::post('sliceUpload','Web\TestController@sliceUpload');//上传
 //Route::get('sliceDownload','Web\TestController@sliceDownload');//下载
 Route::get('api/vranchor/sliceDownload','Api\VideoController@sliceDownload');//视频下载
 //Route::get('list','Web\TestController@list');
+Route::get('web/vranchor/index','Web\IndexController@index');  //首页
+Route::post('web/vranchor/moreslice','Web\IndexController@moreslice'); //更多视频
+Route::post('web/vranchor/search','Web\IndexController@search');//搜索
 Route::group(['prefix'=>'web','middleware'=>'check.login'],function (){
-    Route::get('vranchor/index','Web\IndexController@index');  //首页
+    //Route::get('vranchor/index','Web\IndexController@index');  //首页
     Route::get('vranchor/userlist','Web\UserController@userlist'); //用户列表
     Route::get('vranchor/userinfo','Web\UserController@userinfo'); //用户信息
     Route::post('vranchor/updateinfo','Web\UserController@updateinfo'); //修改用户信息
     Route::post('vranchor/updatepassword','Web\UserController@updatepassword'); //修改密码
-    Route::post('vranchor/moreslice','Web\IndexController@moreslice'); //更多视频
+    //Route::post('vranchor/moreslice','Web\IndexController@moreslice'); //更多视频
     Route::post('vranchor/myslice','Web\IndexController@myslice'); //我的视频
     Route::post('vranchor/mycollect','Web\IndexController@mycollect'); //我的收藏
     Route::post('vranchor/myhistory','Web\IndexController@myhistory'); //历史记录
-    Route::post('vranchor/search','Web\IndexController@search');//搜索
+    //Route::post('vranchor/search','Web\IndexController@search');//搜索
     Route::post('vranchor/cancelcollect','Web\IndexController@cancelcollect'); //取消收藏
     Route::get('vranchor/filetype','Web\IndexController@filetype'); //视频类型
     Route::post('vranchor/orderuserinfo','Web\UserController@orderuserinfo'); //用户信息
